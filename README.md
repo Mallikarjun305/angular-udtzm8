a08594044bc6cda0d742a0579f5dbc1627872410
Mallikarjun305 <mallikarjun854894@gmail.com>
2022-02-17 10:38:09 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-udtzm8
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-udtzm8)
---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'blogdemo';
+  show() {
+    let date = new Date();
+    return date;
+  }
+}
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
